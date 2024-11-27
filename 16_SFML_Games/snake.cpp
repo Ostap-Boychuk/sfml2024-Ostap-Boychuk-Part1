67880b43e2536aa93dd371c969d0bb7f61fdcc9f
Ostap Boychuk <c00301627@setu.ie>
2024-11-27 14:54:28 +0000

    added comments and renamed some variables

--- 16_SFML_Games/snake.cpp
+++ 16_SFML_Games/snake.cpp
@@ -2,57 +2,75 @@
 #include <time.h>
 using namespace sf;
 
-int N=30,M=20; // grid dimentions. N - width, M - height
-int sz=16;
-int w = sz*N;
-int h = sz*M;
+const int GRID_WIDTH = 30;// number of cells in width
+const int GRID_HEIGHT = 20;// number of cells in height 
+const int CELL_SIZE=16; // size of each cell in pixels
+const int WINDOW_WIDTH = CELL_SIZE * GRID_WIDTH; // Total window width in pixels
+const int WINDOW_HEIGHT = CELL_SIZE * GRID_HEIGHT; // total window height in pixels 
 
-int dir,num=4;
+int direction,num=4; // direction of snake movement and initial lenght of the snake
 
-struct Snake 
-{ int x,y;}  s[100];
+struct Snake // structure of snake segment 
+{ 
+    int x,y;
+} snakeSegments[100]; //  snake can have up to 100 segments 
 
-struct Fruit
-{ int x,y;} f;
+struct Fruit // structer of fruit
+{ 
+    int x,y;
+} fruit;
 
-void Tick()
+void Tick() // function that update game state each tick 
  {
+    // move each part of the snake to the position of th part before it 
     for (int i=num;i>0;--i)
-     {s[i].x=s[i-1].x; s[i].y=s[i-1].y;}
+     {snakeSegments[i].x=snakeSegments[i-1].x; snakeSegments[i].y=snakeSegments[i-1].y;}
 
-    if (dir==0) s[0].y+=1;      
-    if (dir==1) s[0].x-=1;        
-    if (dir==2) s[0].x+=1;         
-    if (dir==3) s[0].y-=1;   
+    // update position of the snake head based on the direction
+    if (direction==0) snakeSegments[0].y+=1;  // move down    
+    if (direction==1) snakeSegments[0].x-=1;  // move left      
+    if (direction==2) snakeSegments[0].x+=1;  // move right 
+    if (direction==3) snakeSegments[0].y-=1;  // move up
 
-    if ((s[0].x==f.x) && (s[0].y==f.y)) 
-     {num++; f.x=rand()%N; f.y=rand()%M;}
 
-    if (s[0].x>N) s[0].x=0;  if (s[0].x<0) s[0].x=N;
-    if (s[0].y>M) s[0].y=0;  if (s[0].y<0) s[0].y=M;
+    // check if head of the snake collided with fruit 
+    if ((snakeSegments[0].x==fruit.x) && (snakeSegments[0].y==fruit.y)) 
+     {num++; // increase the lenght of the snake 
+    fruit.x=rand()%GRID_WIDTH; // generate random fruit position  
+    fruit.y=rand()%GRID_HEIGHT;}
+
+    // wrap snake around ths screen edges
+    if (snakeSegments[0].x>GRID_WIDTH) snakeSegments[0].x=0;  
+    if (snakeSegments[0].x<0) snakeSegments[0].x=GRID_WIDTH;
+    if (snakeSegments[0].y>GRID_HEIGHT) snakeSegments[0].y=0;  
+    if (snakeSegments[0].y<0) snakeSegments[0].y=GRID_HEIGHT;
  
+    // check if the snake head collides with its body
     for (int i=1;i<num;i++)
-     if (s[0].x==s[i].x && s[0].y==s[i].y)  num=i;
+     if (snakeSegments[0].x==snakeSegments[i].x && snakeSegments[0].y==snakeSegments[i].y)  num=i;
  }
 
 int snake()
 {  
-    srand(time(0));
+    srand(time(0)); // random seed
 
-    RenderWindow window(VideoMode(w, h), "Snake Game!");
+    // create sfml window 
+    RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Snake Game!");
 
-    Texture t1,t2;
-    t1.loadFromFile("images/snake/white.png");
-    t2.loadFromFile("images/snake/red.png");
+    // load textures for snake and fruit 
+    Texture snakeTexture,fruitTexture;
+    snakeTexture.loadFromFile("images/snake/white.png");
+    fruitTexture.loadFromFile("images/snake/red.png");
 
-    Sprite sprite1(t1);
-    Sprite sprite2(t2);
+    // create sprites for them 
+    Sprite sprite1(snakeTexture);
+    Sprite sprite2(fruitTexture);
 
     Clock clock;
     float timer=0, delay=0.1;
 
-    f.x=10;
-    f.y=10; 
+    fruit.x=10; // set position for the fruit 
+    fruit.y=10; 
     
     while (window.isOpen())
     {
@@ -67,24 +85,24 @@ int snake()
                 window.close();
         }
 
-        if (Keyboard::isKeyPressed(Keyboard::Left)) dir=1;   
-        if (Keyboard::isKeyPressed(Keyboard::Right)) dir=2;    
-        if (Keyboard::isKeyPressed(Keyboard::Up)) dir=3;
-        if (Keyboard::isKeyPressed(Keyboard::Down)) dir=0;
+        if (Keyboard::isKeyPressed(Keyboard::Left)) direction=1;   
+        if (Keyboard::isKeyPressed(Keyboard::Right)) direction=2;    
+        if (Keyboard::isKeyPressed(Keyboard::Up)) direction=3;
+        if (Keyboard::isKeyPressed(Keyboard::Down)) direction=0;
 
         if (timer>delay) {timer=0; Tick();}
 
    ////// draw  ///////
     window.clear();
 
-    for (int i=0; i<N; i++) 
-      for (int j=0; j<M; j++) 
-        { sprite1.setPosition(i*sz, j*sz);  window.draw(sprite1); }
+    for (int i=0; i<GRID_WIDTH; i++) 
+      for (int j=0; j<GRID_HEIGHT; j++) 
+        { sprite1.setPosition(i*CELL_SIZE, j*CELL_SIZE);  window.draw(sprite1); }
 
     for (int i=0;i<num;i++)
-        { sprite2.setPosition(s[i].x*sz, s[i].y*sz);  window.draw(sprite2); }
+        { sprite2.setPosition(snakeSegments[i].x*CELL_SIZE, snakeSegments[i].y*CELL_SIZE);  window.draw(sprite2); }
    
-    sprite2.setPosition(f.x*sz, f.y*sz);  window.draw(sprite2);    
+    sprite2.setPosition(fruit.x*CELL_SIZE, fruit.y*CELL_SIZE);  window.draw(sprite2);    
 
     window.display();
     }
